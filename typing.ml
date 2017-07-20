3ff50379249452ef8e14d7df76c79d09a2bfd13a
芦田聖太 <omushota@h247.183.229.10.1017421.vlan.kuins.net>
2017-07-20 13:10:27 +0900

    unify unify

--- typing.ml
+++ typing.ml
@@ -40,7 +40,7 @@ let rec unify a = match a with
                                                                       else unify ( subst_eqs [(c, TyFun(a,b))] rest ) @ [ (c, TyFun(a,b)) ]
                                           | (TyFun (x1, y1), TyFun (x2, y2)) -> unify( (x1 , x2) :: (y1 ,y2) :: rest )
                                           | (TyVar a, TyVar b) -> unify ( subst_eqs [(a, TyVar b)] rest ) @ [ (a, TyVar b) ]
-                                          | (_, _) -> []
+                                          | (_, _) -> err ("can't unify")
 
 
 
@@ -58,6 +58,20 @@ let ty_prim op ty1 ty2 = match op with
   | Cons -> err "ty_prim Not Implemented!"
 
 
+let rec funChecker a b = match a , b with
+                          TyInt, TyInt ->  ([], true)
+                        | TyBool, TyBool -> ([], true)
+                        | TyVar c, TyVar d -> let s1 = (c, b) in (s1 :: [], true)
+                        | TyInt, TyVar c -> let s1 = (c, a) in (s1 :: [], true)
+                        | TyBool, TyVar c -> let s1 = (c, a) in (s1 :: [], true)
+                        | TyFun(x, y) , TyVar c -> let s1 = (c, a) in (s1 :: [], true)
+                        | TyVar c, TyInt -> let s1 = (c, b) in (s1 :: [], true)
+                        | TyVar c, TyBool -> let s1 = (c, b) in (s1 :: [], true)
+                        | TyVar c, TyFun(x, y) -> let s1 = (c, b) in (s1 :: [], true)
+                        | TyFun (x1, y1), TyFun(x2, y2) -> let s1, bool1 = (funChecker x1 x2) in
+                                                              let s2, bool2 = (funChecker y1 y2) in
+                                                                (s2 @ s1 @ s2 , bool1 && bool2)
+                        | _, _ -> ([], false)
 
 let rec ty_exp tyenv = function
     Var x ->
@@ -97,8 +111,9 @@ let rec ty_exp tyenv = function
                                                         in (eqs2, ty4)
                                 | TyVar p, TyFun (a, b) -> let eqs2 = unify ( eqs_of_subst ((p, ty4) :: eqs1) )
                                                         in (eqs2, ty4)
-                                | TyFun (a, b), TyFun (c, d) -> let eqs3 = unify ((a, c) :: (b, d) :: s3) in
-                                                                  (eqs3, ty3)
+                                | TyFun (a, b), TyFun (c, d) -> let s4, bool1 = funChecker ty3 ty4 in
+                                                                          let s5 = unify (eqs_of_subst (s4 @ eqs1)) in
+                                                                          if bool1 then (s5 ,subst_type s5 ty3) else err("type not match")
 
                                 | _, _ -> err("エラーだよ")
                               )
@@ -127,25 +142,25 @@ let rec ty_exp tyenv = function
                                                             in (eqs2, ty4)
                                     | TyVar p, TyFun (a, b) -> let eqs2 = unify ( eqs_of_subst ((p, ty4) :: eqs1) )
                                                             in (eqs2, ty4)
-                                    | TyFun (a, b), TyFun (c, d) -> let eqs3 = unify ((b, d) :: (a, c) :: (b, d) :: s3) in
-                                                                      (eqs3, subst_type eqs3 ty3)
+                                    | TyFun (a, b), TyFun (c, d) -> let s4, bool1 = funChecker ty3 ty4 in
+                                                                          let s5 = unify (eqs_of_subst (s4 @ eqs1)) in
+                                                                          if bool1 then (s5 ,subst_type s5 ty3) else err("type not match")
                                     | _, _ -> err("エラーだよ")
                                     )
         | (_,_) -> err("ifExp Not Implemented!3")
       )
 
-  | LetExp (id, exp1, exp2) -> let (a, b) = (ty_exp tyenv exp1) in
+  | LetExp (id, exp1, exp2) -> let (a, b) = ty_exp tyenv exp1 in
                                   let (c, d) = ty_exp (Environment.extend id b tyenv) exp2 in
                                     let s = unify( eqs_of_subst (c @ a) ) in
                                     (s, subst_type s d )
-
-
 
   | FunExp (id, exp) -> 
     let domty = TyVar (fresh_tyvar ()) in
     let s, ranty =
       ty_exp (Environment.extend id domty tyenv) exp in
        (s, TyFun (subst_type s domty, ranty))
+
   | AppExp (exp1, exp2) ->  let (s1, ty1) = ty_exp tyenv exp1 in
                             let (s2, ty2) = ty_exp tyenv exp2 in
                             let s3 = unify (eqs_of_subst (s1 @ s2) ) in
@@ -155,7 +170,9 @@ let rec ty_exp tyenv = function
                                 TyFun (TyInt, b) , TyInt -> ( s3, b )
                               | TyFun (TyBool, b) , TyBool -> ( s3, b )
                               | TyFun (TyVar a, b) , c -> let d = unify (eqs_of_subst ((a, c) :: s3) ) in (d, subst_type d b)
-                              | TyFun (TyFun (a, b), c), d -> let e = unify ( (TyFun (a,b), d) :: eqs_of_subst s3) in (e, subst_type e c)
+                              | TyFun (TyFun (a, b), c), TyFun (d, e) -> let s4, bool1 = funChecker (TyFun(a, b)) ty4 in
+                                                                          let s5 = unify (eqs_of_subst (s4 @ s3)) in
+                                                                          if bool1 then (s5 ,subst_type s5 c) else err("TypeError")
                               | TyVar a, TyVar b -> let c = fresh_tyvar () in
                                                     let d = unify (eqs_of_subst (( a, TyFun (TyVar b,  TyVar c) ):: s3)) in
                                                       ( d , subst_type d (TyVar c) )
@@ -169,7 +186,7 @@ let rec ty_exp tyenv = function
                               | TyVar a, TyInt -> let p = fresh_tyvar() in
                                                   let q = unify (eqs_of_subst ((a, TyFun (TyInt , TyVar p)) :: s3)) in
                                                     (q, subst_type q (TyVar p))
-                              | _, _ -> err ("AppExpEror") 
+                              | _, _ -> err ("AppExpError") 
                               )
   | _ -> err ("ty_exp Not Implemented!")
 
