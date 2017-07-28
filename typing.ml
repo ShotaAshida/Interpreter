10710c1856ea23665ea1e852d5ad56b1a68e0e19
芦田聖太 <omushota@omushota-MacBook-Pro.local>
2017-07-28 23:00:54 +0900

    final

--- typing.ml
+++ typing.ml
@@ -11,7 +11,7 @@ let rec subst_type a b = match b with
                         TyVar c ->
                           (match a with
                               [] -> b
-                            | (d, e) :: rest -> if d = c then subst_type a e else subst_type rest b )
+                            | (d, e) :: rest -> if d = c then subst_type rest e else subst_type rest b )
                       | TyFun (f, g) -> TyFun ( subst_type a f, subst_type a g )
                       | _ -> b
 
@@ -22,16 +22,6 @@ let rec eqs_of_subst s = match s with
 let rec subst_eqs s eqs = match eqs with
                           [] -> [] 
                         | (a, b) :: rest -> (subst_type s a, subst_type s b) :: (subst_eqs s rest)
-
-
- (* let rec freevar_tysc tysc = let tyvars, ty = tysc in
-                            match ty with
-                                  TyVar x -> if MySet.member ty tyvars then MySet.empty
-                                                else MySet.singleton x
-                                | TyFun(x, y) -> let tyx = freevar_tysc (tyvars, x) in
-                                                    let tyy = freevar_tysc (tyvars, y) in
-                                                    MySet.union tyx tyy
-                                | _ -> MySet.empty  *)
 
 
 
@@ -61,10 +51,6 @@ let closure ty tyenv subst =
         (fun id -> freevar_ty (subst_type subst (TyVar id)))
         fv_tyenv') in
   let ids = MySet.diff (freevar_ty ty) fv_tyenv in
-      (* let rec printer vars = (match vars with
-                            [] -> print_string("closure:::\n")
-                          | x :: rest -> print_int x; print_string(" ") ; printer rest)
-    in printer (MySet.to_list fv_tyenv');   *)
   TyScheme (MySet.to_list ids, ty)
 
 let rec unify a = 
@@ -104,10 +90,6 @@ let rec ty_exp tyenv = function
     Var x ->
       (try 
        let TyScheme (vars, ty) = Environment.lookup x tyenv in
-          (* let rec printer vars = (match vars with
-                            [] -> print_string("var:::\n")
-                          | x :: rest -> print_int x; print_string(" ") ; printer rest)
-         in printer vars;    *)
         let s = List.map (fun id -> (id, TyVar (fresh_tyvar ())))
           vars in  ([], subst_type s ty)
         with Environment.Not_bound -> err ("variable not bound: " ^ x))
@@ -118,9 +100,7 @@ let rec ty_exp tyenv = function
       let (s2,ty2) = ty_exp tyenv exp2 in
       let (eqs3, ty) = ty_prim op ty1 ty2 in
       let eqs = (eqs_of_subst s1) @ (eqs_of_subst s2) @ eqs3 in
-      (* print_string("bin1"); *)
       let s3 = unify eqs in 
-      (* print_string("bin2\n"); *)
       (s3, subst_type s3 ty)
   | IfExp (exp1, exp2, exp3) ->
       let (s1, ty1) = ty_exp tyenv exp1 in
@@ -130,19 +110,14 @@ let rec ty_exp tyenv = function
       let eqs2 = eqs_of_subst s2 in
       let eqs3 = eqs_of_subst s3 in
       let eqs = eqs1 @ eqs2 @ eqs3 @ [(ty2, ty3) ; (ty1, TyBool)] in
-      (* print_string("ifin"); *)
       let s4 = unify eqs in
-       (* print_string("ifout\n"); *)
        (s4, subst_type s4 ty2) 
 
 
   | LetExp (id, exp1, exp2) ->  let (a, b) = ty_exp tyenv exp1 in
                                 let tysc = closure b tyenv a in
-                                  (* print_string("letexp "); tyscPrinter tysc;   *)
                                 let (c, d) = ty_exp (Environment.extend id tysc tyenv) exp2 in
-                                (* print_string("funin"); *)
                                 let s = unify( eqs_of_subst (c @ a) ) in 
-                                (* print_string("funout"); *)
                                 (s, subst_type s d )
 
   | FunExp (id, exp) -> 
@@ -157,26 +132,19 @@ let rec ty_exp tyenv = function
                             let eqs2 = eqs_of_subst s2 in
                             let midty1 = TyVar (fresh_tyvar()) in
                             let eqs = eqs1 @ eqs2 @ [(TyFun (ty2, midty1) , ty1)] in
-                            (* print_string("appin"); *)
                             let s3 = unify eqs in
-                             (* print_string("appout\n"); *)
                              (s3, subst_type s3 midty1) 
   
   | LetRecExp (id, para, exp1, exp2) -> let dummyTy = TyVar( fresh_tyvar() ) in
-                                        let var = fresh_tyvar() in
-                                        let ty1 = TyVar( var ) in
-                                        let newenv1 = Environment.extend para (TyScheme([], ty1)) (Environment.extend id (TyScheme([], dummyTy)) tyenv) in
-                                        (* print_string("rec1in\n"); *)
-                                        let (s1, ty2) = ty_exp newenv1 exp1 in
-                                        (* print_string("rec1out\n"); *)
-                                        let eqs = [(dummyTy, TyFun(ty1, ty2))] in
-                                        let subst1 = unify (eqs @ (eqs_of_subst s1)) in
+                                        let paraty1 = TyVar( fresh_tyvar() ) in
+                                        let dummyEnv = Environment.extend para (TyScheme([], paraty1)) (Environment.extend id (TyScheme([], dummyTy)) tyenv) in
+                                        let (s1, ty2) = ty_exp dummyEnv exp1 in
+                                        let eqs = [(dummyTy, TyFun(paraty1, ty2))] in
+                                        let subst1 = unify ((eqs_of_subst s1) @ eqs) in
                                         let ty3 = subst_type subst1 dummyTy in
-                                        let tysc = closure ty3 newenv1 subst1 in 
-                                        (* print_string("rec2in\n");  *)
-                                        let (s2, ty4) = ty_exp (Environment.extend id tysc newenv1) exp2 in
-                                        (* print_string("rec2out\n"); *)
-                                        let subst2 = unify(eqs_of_subst (subst1 @ s2)) in  (subst2, subst_type subst2 ty4)
+                                        let tysc = closure ty3 tyenv s1 in
+                                        let (s3, ty5) = ty_exp (Environment.extend id tysc tyenv) exp2 in
+                                        let subst2 = unify(eqs_of_subst (subst1 @ s3)) in  (subst2, subst_type subst2 ty5)
 
   | _ -> err ("ty_exp Not Implemented!")
 
@@ -187,11 +155,11 @@ let ty_decl tyenv = function
   | DeclDecl (id, e1, e2) -> let (subst, v) = ty_exp tyenv e1 in
                                 let tysc = closure v tyenv subst in (v, Environment.extend id tysc tyenv)
   | RecDecl (id, para, exp) ->  let dummyTy = TyVar( fresh_tyvar() ) in
-                                let ty1 = TyVar( fresh_tyvar() ) in
-                                let newenv1 = Environment.extend para (TyScheme([], ty1)) (Environment.extend id (TyScheme([], dummyTy)) tyenv) in
+                                let paraty1 = TyVar( fresh_tyvar() ) in
+                                let newenv1 = Environment.extend para (TyScheme([], paraty1)) (Environment.extend id (TyScheme([], dummyTy)) tyenv) in
                                 let (s1, ty2) = ty_exp newenv1 exp in
-                                let eqs = [(dummyTy, TyFun(ty1, ty2))] in
+                                let eqs = [(dummyTy, TyFun(paraty1, ty2))] in
                                 let subst1 = unify (eqs @ (eqs_of_subst s1)) in
                                 let ty3 = subst_type subst1 dummyTy in
-                                let tysc = closure ty3 newenv1 subst1 in (ty3, Environment.extend id tysc newenv1)
+                                let tysc = closure ty3 tyenv s1 in (ty3, Environment.extend id tysc tyenv)
   | _ -> err ("ty_decl Not Implemented!")
